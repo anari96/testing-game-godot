dd1b2b6da9396e412e16ed70cdbea4fde3b5563b
anari96 <nabilrivaldy@gmail.com>
2024-05-07 04:57:07 +0800

    added item type check

--- script/item.gd
+++ script/item.gd
@@ -5,9 +5,11 @@ enum ITEM_TYPE {CONSUMABLE,KEY,EQUIPMENT}
 @export var name:String = "item_name"
 @export var type: ITEM_TYPE = ITEM_TYPE.CONSUMABLE
 @export var description: String = "Description"
-@export var heal_value: int = 0
 @export var effect : Resource = load("res://script/item/item_effect_base.gd").new()
 
 func use() -> void:
 	print("used item : " + name)
 	effect.effect()
+
+func get_type_key() -> String:
+	return ITEM_TYPE.keys()[type]
--- script/menu/item.gd
+++ script/menu/item.gd
@@ -7,7 +7,7 @@ var item_count:int = 1
 
 func _ready():
 	$item_name.pressed.connect(_on_item_pressed)
-	item_count_label.text = str(item_count)	
+	item_count_label.text = str(item_count)
 	item_name_label.text = str(item.name)
 
 func use():
@@ -17,12 +17,12 @@ func remove():
 	queue_free()
 
 func _on_item_pressed():
-	use()
-	InventoryManager.remove_item(item.name)
-	if item_count == 1:
-		
-		queue_free()
-	else:
-		item_count -= 1
-		item_count_label.text = str(item_count)
-		item_name_label.text = str(item.name)
+	if item.get_type_key() != "KEY":
+		use()
+		InventoryManager.remove_item(item.name)
+		if item_count == 1:
+				queue_free()
+		else:
+			item_count -= 1
+			item_count_label.text = str(item_count)
+			item_name_label.text = str(item.name)
